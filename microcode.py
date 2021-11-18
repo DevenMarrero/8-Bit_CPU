2c9063f98110416ebad40f8e66e3c65353b283c3
Deven Marrero <devensmarrero@gmail.com>
2021-11-17 22:58:30 -0800

    Microcode working without flags

--- microcode.py
+++ microcode.py
@@ -1,36 +1,66 @@
 import yaml
 
-def address_in_microcode(address, microcode):
+def get_opcode(address, microcode):
     for opcode in microcode["instructions"]:
-        if microcode["instruction"][opcode]["value"] == address:
-            return True
-    return False
+        if microcode["instructions"][opcode]["value"] == address:
+            return opcode
+    return None
+
+
+def or_list(nums):
+    number = 0
+    for num in nums:
+        number = number | num
+    return number
 
 
 with open("microcode.yaml", 'rb') as file:
     microcode = yaml.safe_load(file)
 
 control_lines = microcode["control_lines"]
-# 262143
-for address in range(255):
-    #   SEL Flag   Inst   Step      
-    # 0 0   0000 00000000 0000
-    step = address & 0b000000000000001111
-    instruction = (address & 0b000000111111110000) >> 4
-    flag = (address & 0b001111000000000000) >> 12
-    bitSel = (address & 0b010000000000000000) >> 16
-    print(bin(address), bitSel, flag, instruction, step)
 
-    if step < 3:
-        micro = (microcode["instruction_prefix_steps"][str(step)])
-    break
+print("Generating Microcode ROM")
 
-    if address_in_microcode(address, microcode):
-        pass
-    else:
-        pass
+with open(microcode["output_file_name"], "wb") as out_file:
+    # 262143
+    for address in range(262143):
+        #   SEL Flag   Inst   Step      
+        # 0 0   0000 00000000 0000
+        step = address & 0b000000000000001111
+        instruction = (address & 0b000000111111110000) >> 4
+        flag = (address & 0b001111000000000000) >> 12
+        byte_sel = (address & 0b100000000000000000) >> 17
+        
+        #print(f"\n{get_opcode(instruction, microcode)} Step: {step}")
+        #print(bin(address), byte_sel, flag, instruction, step)
 
-    # with open(microcode["output_file_name"], "wb") as out_file:
-    #     out_file.write()
+        micro = None
+        # First 3 steps are always PCO|MLI, PRO|MHI, MDO|II
+        if step < 3:
+            micro = microcode["instruction_prefix_steps"][step]
+        else:
+            # If the opcode exists
+            if opcode := get_opcode(instruction, microcode):
+                
+                # If the step exists
+                micro = microcode["instructions"][opcode]["steps"].get(step)
 
+            # Otherwise just zero fill
+            if micro is None:
+                micro = [{'lines': [0]}]
 
+        bytes = []
+        for control_line in micro[0]["lines"]:
+            bytes.append(control_lines[control_line])
+            
+        byte = or_list(bytes)
+
+        #print("Micro:", micro, "Bits:", byte)
+        if not byte_sel:
+            out_file.write((byte & 0xFFFF).to_bytes(2, byteorder='big'))
+        else:
+            out_file.write((byte >> 16).to_bytes(2, byteorder='big'))
+
+        if address % 256 == 0:
+            print(".", end="")
+print(" done")

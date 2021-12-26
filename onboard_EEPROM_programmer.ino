6237c3a39c1c0e3263ed6ee668e37a4e425eddb3
Deven Marrero <devensmarrero@gmail.com>
2021-12-25 23:58:06 -0800

    Update onboard_EEPROM_programmer.ino

--- onboard_EEPROM_programmer.ino
+++ onboard_EEPROM_programmer.ino
@@ -1,4 +1,4 @@
-# include <Arduino_DebugUtils.h>
+// # include <Arduino_DebugUtils.h>
 
 # define DATA_0 2
 # define DATA_7 9
@@ -9,78 +9,103 @@
 // Digital active low
 # define MLI 10
 # define MHI 11
-# define CLK 12
-# define MDI 13
+# define MDI 12
+# define CLK 13
 
 int address = 0;
-int data;
+int data = 0;
 bool programming = false;
+bool write_high;
+
 
 
 void writeEEPROM(int address, int data){
+  if ((address & 0b11111111) == 0){
+    write_high = true;
+  }
+  else {
+    write_high = false;
+  }
+  
   // low address
-  for (int pin = DATA_0; pin < DATA_7; pin++){
+  for (int pin = DATA_0; pin <= DATA_7; pin++){
     digitalWrite(pin, address & 1);
     address = address >> 1;
   }
   digitalWrite(MLI, LOW);
+  delayMicroseconds(1);
   digitalWrite(CLK, LOW);
+  delayMicroseconds(1);
   digitalWrite(CLK, HIGH);
   digitalWrite(MLI, HIGH);
 
-  // high address
-  for (int pin = DATA_0; pin < DATA_7; pin++){
-    digitalWrite(pin, address & 1);
-    address = address >> 1;
+  
+  if (write_high){
+
+    // high address
+    for (int pin = DATA_0; pin <= DATA_7; pin++){
+      digitalWrite(pin, address & 1);
+      address = address >> 1;
+    }  
+    delay(100);
+    digitalWrite(MHI, LOW);
+    delayMicroseconds(10);
+    digitalWrite(CLK, LOW);
+    delayMicroseconds(1);
+    digitalWrite(CLK, HIGH);
+    digitalWrite(MHI, HIGH);
   }
-  digitalWrite(MHI, LOW);
-  digitalWrite(CLK, LOW);
-  digitalWrite(CLK, HIGH);
-  digitalWrite(MHI, HIGH);
 
   // data
-  for (int pin = DATA_0; pin < DATA_7; pin++){
+  for (int pin = DATA_0; pin <= DATA_7; pin++){
     digitalWrite(pin, data & 1);
     data = data >> 1;
-  }
+  }  
+  //delay(1);
   digitalWrite(MDI, LOW);
+  delayMicroseconds(1);
   digitalWrite(CLK, LOW);
+  delayMicroseconds(1);
   digitalWrite(CLK, HIGH);
   digitalWrite(MDI, HIGH);
 }
 
 
 void resetLines(){
+  digitalWrite(CLK, HIGH);
+
   // set lines to high-z
   for (int pin = DATA_0; pin <= DATA_7; pin++){
     pinMode(pin, INPUT);
   }
 
-  pinMode(MLI, INPUT);
-  pinMode(MHI, INPUT);
-  pinMode(MDI, INPUT);
+  digitalWrite(MDI, HIGH);
+  digitalWrite(MLI, HIGH);
+  digitalWrite(MHI, HIGH);
+
   pinMode(CLK, INPUT);
 
   analogWrite(control_CE, 0); // enable control eproms
 }
 
 void setLines(){
-  analogWrite(control_CE, 255); // dsiable control eproms
-  
-  // set data pins to output
-  for (int pin = DATA_0; pin <= DATA_7; pin++){
-    pinMode(pin, OUTPUT);
-  }
   
   digitalWrite(MLI, HIGH);
   digitalWrite(MHI, HIGH);
   digitalWrite(MDI, HIGH);
   digitalWrite(CLK, HIGH);
 
+  analogWrite(control_CE, 255); // dsiable control eproms
+
   pinMode(MLI, OUTPUT);
   pinMode(MHI, OUTPUT);
   pinMode(MDI, OUTPUT);
   pinMode(CLK, OUTPUT);
+
+  // set data pins to output
+  for (int pin = DATA_0; pin <= DATA_7; pin++){
+    pinMode(pin, OUTPUT);
+  }
 }
 
 
@@ -97,6 +122,7 @@ void loop() {
   if (Serial.peek() == '<'){
     programming = true;
     setLines();
+    delay(10);
     Serial.println(Serial.read());
   }
   else if (Serial.peek() == '>'){
@@ -105,7 +131,7 @@ void loop() {
     Serial.println(Serial.read());
   }
 
-  if (programming){
+  else if (programming){
     // serial input starts here
     data = Serial.parseInt();
     writeEEPROM(address, data); // Write data to Eprom
@@ -113,6 +139,8 @@ void loop() {
     Serial.read(); // clear input buffer
     address++;
     //Debug.print(DBG_INFO, "High = %d, Low = %d, data = %d", address >> 8, address & 0b11111111, data);
+    delay(100);
     Serial.println(data); // ready for next byte
+    
   }
 }

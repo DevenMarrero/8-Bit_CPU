385ff0668385afaee8dce90dac07c9a33c36f49b
Deven Marrero <devensmarrero@gmail.com>
2022-01-12 15:33:27 -0800

    Update 0-255.asm

--- 0-255.asm
+++ 0-255.asm
@@ -3,18 +3,21 @@
 ; hardware const
 HEXOUT = 0x8000
 
-move a, #0
+move a, #0 ; clear a register
 
 inc_loop:
     store [HEXOUT], a ; output a
-    inc a
+    inc a 
 
-    cmp #255
+    cmp #255 ; jump to dec_loop if a = 255
     jz dec_loop
-    jmp inc_loop
+    
+    jmp inc_loop ; otherwise go back to inc_loop
 
 dec_loop:
-    store [HEXOUT], a 
+    store [HEXOUT], a ; output a
     dec a
-    jz inc_loop
-    jmp dec_loop
+    
+    jz inc_loop ; jump to inc_loop if a = 0
+    
+    jmp dec_loop ; otherwise go back to dec_loop

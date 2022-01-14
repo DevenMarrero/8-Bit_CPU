0dd00a7a9ee200b0072c552f35433864ac64fec2
Deven Marrero <devensmarrero@gmail.com>
2022-01-13 19:20:17 -0800

    Added comments

--- ship.asm
+++ ship.asm
@@ -6,10 +6,11 @@ LCDOUT = 0x8010
 LCDSET = 0x8020
 INPUT =  0x8030
 
-E =  0b00000001
-RW = 0b00000010
-RS = 0b00000100
+E =  0b00000001 ; lcd enable pin
+RW = 0b00000010 ; read/write
+RS = 0b00000100 ; register select
 
+; const
 arrow = 0b01111110
 block = 0b11111111
 
@@ -49,6 +50,7 @@ reset:
     mov a, #0b00000001 ; clear disoplay
     call lcd_instruction
 
+    ; wait for lcd to clear
     call reset_clock
     wait:
         call clock
@@ -75,6 +77,7 @@ start:
     mov a, #startMsg >> 8
     store [screenInd], a
     call printMsg
+    ; loop until any button is pressed
     .wait:
         load a, [INPUT]
         bit #0xFF
@@ -113,10 +116,12 @@ set_input:
     bit #0b00000100
     jnz .down
     ret
+    ; up button pressed
     .up:
         mov b, #1 
         store [shipUp], b
         ret
+    ; down button pressed
     .down:
         mov b, #0
         store [shipUp], b
@@ -124,7 +129,7 @@ set_input:
 
 ; renders screen
 draw_lcd:
-    ; init
+    ; clear lcd
     mov b, #0
     mov a, #0b10000000
     call lcd_instruction
@@ -132,7 +137,7 @@ draw_lcd:
         load a, [screen], b ; get the next char
         bit #0xFF ; check for end of string
         jz .ret
-        call print_char
+        call print_char ; print the char
         inc b
         ; check to start on next line
         mov a, b
@@ -152,12 +157,12 @@ printMsg:
     call clear_screen
     mov c, #0
     .print:
-        load b, [screenInd]
-        load a, [bc]
-        mov b, #screen >> 8
+        load b, [screenInd] ; start value of string 
+        load a, [bc] ; load char to a
+        mov b, #screen >> 8 ; paste value to screen meory
         store [bc], a 
 
-        inc c
+        inc c ; next char
         mov a, c 
         cmp #0x20
         jnz .print
@@ -166,14 +171,16 @@ printMsg:
     ret
 ; clear screen memory
 clear_screen:
+    ; go to start of screen memory
     mov b, #screen >> 8
     mov c, #screen - 1 & 0xFF
+    ; loop through each byte and write " "
     .clear:
         inc c
         mov a, #" "
         store [bc], a
         mov a, c
-        cmp #0x1F
+        cmp #0x1F ; end of memory
         jnz .clear
     mov a, #0
     store [screen + 0x20], a 
@@ -204,40 +211,45 @@ draw_block:
 ; moves single block to the left and resets counter
 mov_block:
     load a, [blockPos]
-    sub #1
+    sub #1 ; subtract 1 from block index
     store [blockPos], a
-    call reset_clock
+    call reset_clock ; reset timer for when to move block again
 
-    call block_collision
+    call block_collision ; test if block hit player
     ret
 
 ; controls point and game over
 block_collision:
+    ; block off in top row
     load a, [blockPos]
     cmp #0xFF
     jz .up
 
+    ; block off in bottom row
     cmp #0xF
     jz .down
     ret
+    ; check if ship is also up
     .up:
         load a, [shipUp]
         bit #0xFF
         jnz .end
         jmp .point
-
+    ; check if ship is also down
     .down:
         load a, [shipUp]
         bit #0xFF
         jz .end
         jmp .point
 
+    ; block has not hit player
     .point:
         inc [score]
         call reset_block
-        call update_game_speed
+        call update_game_speed ; check for speed increase
         ret
 
+    ; block hit player
     .end:
         jmp gameOver
         
@@ -285,8 +297,8 @@ update_game_speed:
     cmp #70
     jz .dec
 
-
     ret
+    ; increase speed
     .dec:
         load a, [gameSpeed]
         sub #1
@@ -295,6 +307,7 @@ update_game_speed:
 
 ; display end msg
 gameOver:
+    ; display game over message
     mov a, #gameOverMsg >> 8
     store [screenInd], a
     call printMsg
@@ -355,11 +368,11 @@ print_char:
     ;call lcd_wait
     store [LCDOUT], A
     
-    mov a, #RS
+    mov a, #RS ; select data register
     store [LCDSET], a
-    mov a, #RS|E
+    mov a, #RS|E ; turn on enable
     store [LCDSET], a
-    mov a, #RS
+    mov a, #RS ; turn off enable
     store [LCDSET], a
     ret
 
